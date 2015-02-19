3e51e898f2094ca5fe0d1fd81a158ded0760365b
Marcos Garcia <marcosga.consultant@herbalife.com>
2015-02-19 12:25:08 -0800

    first commit

---
+++ mapAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  mapAnnotation.h
+//  TurismoST
+//
+//  Created by Claudia Oliva on 25/01/12.
+//  Copyright (c) 2012 ASTA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mapAnnotation : NSObject
+
+@end
---
+++ mapAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  mapAnnotation.m
+//  TurismoST
+//
+//  Created by Claudia Oliva on 25/01/12.
+//  Copyright (c) 2012 ASTA. All rights reserved.
+//
+
+#import "mapAnnotation.h"
+
+@implementation mapAnnotation
+
+@end
